a5987f70c55da4ba033195f999062b3ca04ea83b
wang.kuanchih@gmail.com <wang.kuanchih@gmail.com>
2023-06-17 14:47:53 +0000

    added function of mounting adls to databricks

--- set_up/1.mount_adls_with_service_principal.py
+++ set_up/1.mount_adls_with_service_principal.py
@@ -9,4 +9,28 @@
 
 # COMMAND ----------
 
+def mount_adls(storage_account_name: str, container_name: str):
+    client_id = dbutils.secrets.get(scope='mlb-scope', key='mlb-app-AD-client-id')
+    tenant_id = dbutils.secrets.get(scope='mlb-scope', key='mlb-app-AD-tenant-id')
+    client_secret = dbutils.secrets.get(scope='mlb-scope', key='mlb-app-AD-client-secret')
 
+    configs = {"fs.azure.account.auth.type": "OAuth",
+                "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+                "fs.azure.account.oauth2.client.id": client_id,
+                "fs.azure.account.oauth2.client.secret": client_secret,
+                "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
+    
+    for each in dbutils.fs.mounts():
+        if f'/mnt/{storage_account_name}/{container_name}' == each.mountPoint:
+            dbutils.fs.unmount(f'/mnt/{storage_account_name}/{container_name}')
+    
+    dbutils.fs.mount(
+                    source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+                    mount_point = f"/mnt/{storage_account_name}/{container_name}",
+                    extra_configs = configs)
+    
+    display(dbutils.fs.mounts())
+
+# COMMAND ----------
+
+mount_adls('mlbanalysisdl' ,'raw')
